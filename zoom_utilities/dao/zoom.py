a6f8a946c92a196f1922dba91be3028e9700c5ed
Jim Laney <jlaney@uw.edu>
2023-10-02 09:59:28 -0700

    add license

--- zoom_utilities/dao/zoom.py
+++ zoom_utilities/dao/zoom.py
@@ -1,3 +1,6 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from uw_zoom.accounts import Accounts
 from uw_zoom.users import Users
 from uw_zoom.models import ZoomUser
